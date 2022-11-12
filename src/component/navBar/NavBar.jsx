9db132f8e6aae8bf79bbcde757bfb27f3f955cce
Tolu Olofinjana <Tolujana@gmail.com>
2022-11-12 03:02:27 +0100

    added currency switcher functionality

--- src/component/navBar/NavBar.jsx
+++ src/component/navBar/NavBar.jsx
@@ -8,19 +8,45 @@ import { getMenuItems } from "../../utils/queries";
 import { Link } from "react-router-dom";
 import QueryComponent from "../queryComponent/QueryComponent";
 import CartOverlay from "../cartOverlay/CartOverlay";
+import { connect } from "react-redux";
+import { updateCurrency } from "../../Redux/currencySlice";
 
+let currencyDetails = { currencyOptions: null, defaultCurrency: null };
+
+const mapStateToProps = (state) => {
+  return {
+    currencyIndex: state.currency,
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    updateCurrency: (payload) => dispatch(updateCurrency(payload)),
+  };
+};
 export class NavBar extends Component {
   constructor(props) {
     super(props);
     this.menu = React.createRef();
     this.cartItem = React.createRef();
+
     this.state = {
       shouldShowSwitcher: false,
       showCart: this.props.overlay,
       error: false,
       loading: false,
       data: "free",
+      shouldShowCurrency: false,
+      currencies: [],
+      currencyDetails: currencyDetails,
     };
+
+    this.switchCurrency = (e) => {
+      const currency = e.target.getAttribute("value");
+      this.props.updateCurrency(currency);
+      this.setState({ currentCurrency: currency });
+    };
+
     this.switchMenu = (e) => {
       //instead of document.queryselector, use ref.queryselector  instead
       let menuItems = this.menu.current.querySelectorAll(".menu-item");
@@ -36,10 +62,14 @@ export class NavBar extends Component {
       this.props.triggerOverlay();
     };
 
+    this.displayCurrencyOptions = () => {
+      this.setState({ shouldShowCurrency: !this.state.shouldShowCurrency });
+    };
+
     this.loadData = (data) => {
       const { categories } = data;
 
-      return categories.map((category, index) => (
+      return categories?.map((category, index) => (
         <Link
           key={category.name}
           className="link"
@@ -55,20 +85,50 @@ export class NavBar extends Component {
         </Link>
       ));
     };
+
+    this.loadCurrency = (data) => {
+      const { currencies } = data;
+      const { currencyIndex } = this.props.currencyIndex;
+      console.log(this.props);
+      return (
+        <>
+          <div className="currency-icon">
+            <div>{currencies[currencyIndex ?? 0]?.symbol}</div>
+            <img src={arrowDown} alt="" className="arrow-down" />
+          </div>
+          <div
+            className={`currency-options ${
+              this.state.shouldShowCurrency ? "" : "none"
+            }`}
+          >
+            {currencies?.map(({ symbol, label }, index) => (
+              <li
+                key={index}
+                className="currency"
+                onClick={this.switchCurrency}
+                value={index}
+              >{`${symbol} ${label}`}</li>
+            ))}
+          </div>
+        </>
+      );
+    };
   }
-  componentDidUpdate() {}
+  componentDidUpdate(prevProps, prevState) {}
 
   render() {
     return (
       <div className="nav-bar">
         <div ref={this.menu} className="menu-items">
-          <QueryComponent query={getMenuItems} loadData={this.loadData} />{" "}
+          <QueryComponent query={getMenuItems} loadData={this.loadData} />
         </div>
         <img src={logo} alt="" className="logo" />
         <div className="actions">
-          <div className="currency-switcher">
-            <span className="currency-icon">$</span>
-            <img src={arrowDown} alt="" className="arrow-down" />
+          <div
+            className="currency-switcher"
+            onClick={this.displayCurrencyOptions}
+          >
+            <QueryComponent query={getMenuItems} loadData={this.loadCurrency} />
           </div>
           <div
             className="empty-cart"
@@ -91,4 +151,4 @@ export class NavBar extends Component {
   }
 }
 
-export default NavBar;
+export default connect(mapStateToProps, mapDispatchToProps)(NavBar);
