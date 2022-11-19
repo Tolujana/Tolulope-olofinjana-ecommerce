d5595e0db50a786fd13b5ae20899054883ca3584
Tolu Olofinjana <Tolujana@gmail.com>
2022-11-19 12:04:24 +0100

    cleaned cart oVerlay

--- src/component/cartOverlay/CartOverlay.jsx
+++ src/component/cartOverlay/CartOverlay.jsx
@@ -13,7 +13,28 @@ export class CartOverlay extends Component {
     super(props);
 
     this.state = {};
+
+    this.getTotal = (items, currencyIndex) => {
+      const Total = Object.keys(items).reduce((reducer, key, index) => {
+        const { prices } = items[key].productDetails;
+        const { quantity } = items[key];
+
+        return reducer + quantity * prices[currencyIndex ?? 0].amount;
+      }, 0);
+
+      return Total.toFixed(2);
+    };
+
+    this.displayCartItems = (items) => {
+      const cartItem = Object.keys(items).map((key, index) => {
+        return (
+          <CartItem item={items[key]} key={index} cssname="cart-overlay" />
+        );
+      });
+      return cartItem;
+    };
   }
+
   render() {
     console.log(this.props.cartItem);
     const {
@@ -22,32 +43,13 @@ export class CartOverlay extends Component {
     } = this.props;
     return (
       <div className="cart-overlay-wrapper">
+        <div className="my-bag">My Bag:</div>
         <div className="cart-details">
-          <div className="my-bag">My Bag:</div>
-          <div className="items">
-            {Object.keys(items).map((key, index) => {
-              return (
-                <CartItem
-                  item={items[key]}
-                  key={index}
-                  cssname="cart-overlay"
-                />
-              );
-            })}
-          </div>
+          <div className="items">{this.displayCartItems(items)}</div>
         </div>
         <div className="total-detail">
           <div className="total">Total</div>
-          <div className="amount">
-            {Object.keys(items).reduce((reducer, key, index) => {
-              const { prices } = items[key].productDetails;
-
-              return (
-                reducer +
-                items[key].quantity * prices[currencyIndex ?? 0].amount
-              );
-            }, 0)}
-          </div>
+          <div className="amount">{this.getTotal(items, currencyIndex)}</div>
         </div>
         <div className="buttons">
           <Link to="/cart">
